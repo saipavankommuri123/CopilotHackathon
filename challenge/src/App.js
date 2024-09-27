76a08ccf4a85bb2b1e152936de10ef9c6a3044af
saipavankommuri <saipavankommuri123@gmail.com>
2024-09-27 15:15:59 +0530

    initial comming of FE app

---
+++ challenge/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import HomePage from './Home';
+
+const App = () => {
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
+}
+
+export default App;
---
+++ challenge/src/Home.jsx
@@ -0,0 +1,12 @@
+// HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            Welcome Sai
+        </div>
+    );
+}
+
+export default HomePage;
